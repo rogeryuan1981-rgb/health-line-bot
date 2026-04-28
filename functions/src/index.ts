d3ee02e61ae157f640b0b9a0e8642dbf4e102587
rogeryuan1981-rgb <rogeryuan1981@gmail.com>
2026-04-28 18:55:30 +0800

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,10 @@ import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import { handleWebhook } from "./controllers/webhookController";
 
-admin.initializeApp();
+// 只有在還沒開機的情況下才執行開機
+if (!admin.apps.length) {
+    admin.initializeApp();
+}
 
 // 設定在台灣機房 (asia-east1) 執行
 export const webhook = onRequest({ region: "asia-east1" }, async (req, res) => {
