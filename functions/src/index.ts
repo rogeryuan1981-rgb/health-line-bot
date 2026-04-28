a2e094332be4610333e74ab6e4567bb0649f3d6c
rogeryuan1981-rgb <rogeryuan1981@gmail.com>
2026-04-28 17:14:48 +0800

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,10 @@
 import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
-// 確保路徑與您的檔案結構一致
-import { handleWebhook } from "./controllers/webhookController"; 
+import { handleWebhook } from "./controllers/webhookController";
 
 admin.initializeApp();
 
-// 新版 v2 的寫法：將 region 放在設定物件中
+// 設定在台灣機房 (asia-east1) 執行
 export const webhook = onRequest({ region: "asia-east1" }, async (req, res) => {
     await handleWebhook(req, res);
 });
